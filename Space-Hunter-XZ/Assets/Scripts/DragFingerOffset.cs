1e9d689460fe70e245cd6f7fec608811d70c8939
jaggermeista <63716045+jaggermeista@users.noreply.github.com>
2020-04-17 12:52:41 +0200

    Heeeej

---
+++ Space-Hunter-XZ/Assets/Scripts/DragFingerOffset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFingerOffset : MonoBehaviour
+{
+    private float deltaX, deltaY;
+    private Rigidbody2D rigidbody;
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update(){
+
+        if (Input.touchCount>0){
+            
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase){
+                case TouchPhase.Began:
+                    deltaX = touchPos.x - transform.position.x;
+                    deltaY = touchPos.y - transform.position.y;
+                    break;
+                
+                case TouchPhase.Moved:
+                    rigidbody.MovePosition(new Vector2(touchPos.x-deltaX, touchPos.y - deltaY));
+                    break;
+
+                case TouchPhase.Ended:
+                    rigidbody.velocity = Vector2.zero;
+                    break;
+            }    
+        }
+    }
+}
