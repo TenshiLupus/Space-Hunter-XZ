200d355928d8b16c521b250969d6e142e2bc0e02
jaggermeista <63716045+jaggermeista@users.noreply.github.com>
2020-04-17 14:50:55 +0200

    bob

---
+++ Space-Hunter-XZ/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    void Start(){
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed; 
+        DestroyObjectDelayed();
+    }
+
+        void DestroyObjectDelayed()
+    {
+        // Kills the game object in X seconds after loading the object
+        Destroy(gameObject, 1);
+    }
+}
---
+++ Space-Hunter-XZ/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Time.time>nextFire);{
+            nextFire = Time.time + fireRate;
+            Instantiate (shot, shotSpawn.position, shotSpawn.rotation );
+        }
+
+    }
+}
