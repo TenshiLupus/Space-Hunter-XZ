d12688f996f5618b0d9ffba374421f2644b04c4b
Samme <31141459+samkseg@users.noreply.github.com>
2020-05-04 16:10:33 +0200

    Menu

---
+++ Space-Hunter-XZ/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Changelevel(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
