72bbbcc89d6ec9aa8bc2ff7869cf3078383ef730
Samme <31141459+samkseg@users.noreply.github.com>
2020-05-05 02:06:29 +0200

    GameControllerEndastEnPickUpPerVåg

--- Space-Hunter-XZ/Assets/Scripts/GameController.cs
+++ Space-Hunter-XZ/Assets/Scripts/GameController.cs
@@ -7,14 +7,18 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public GameObject[] hazards;
+    public GameObject advEnemy;
     public Vector3 spawnValues;
+    public Vector3 spawnLeft;
+    public Vector3 spawnRight;
     public int hazardCount;
     public int waveCount;
     public int waveIncrease;
     public float spawnWait;
     public float startWait;
+    public float advWaveWait;
     public float waveWait;
-    
+
     public Text scoreText;
     public Text gameOverText;
     public GameObject restartButton;
@@ -25,23 +29,38 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        gameOver = false;;
+        gameOver = false; ;
         gameOverText.text = "";
         score = 0;
         wave = 1;
         UpdateScore();
-        StartCoroutine (SpawnWaves());
+        StartCoroutine(SpawnWaves());
     }
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
         while (true)
         {
+            int hazardsIndex;
+            bool shieldSpawned = false;
             for (int i = 0; i < hazardCount; i++)
             {
                 if (gameOver == false)
                 {
-                    GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                    hazardsIndex = Random.Range(0, hazards.Length);
+                    GameObject hazard;
+                    if (shieldSpawned)
+                    {
+                        hazard = hazards[Random.Range(0, hazards.Length - 1)];
+                    }
+                    else
+                    {
+                        hazard = hazards[hazardsIndex];
+                    }
+                    if (hazardsIndex == 8)
+                    {
+                        shieldSpawned = true;
+                    }
                     Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                     Quaternion spawnRotation = Quaternion.identity;
                     Instantiate(hazard, spawnPosition, spawnRotation);
