1399acc12bd34ad93df41aadc8825873c3f94683
TenshiLupus <57836113+TenshiLupus@users.noreply.github.com>
2020-05-28 15:49:58 +0200

    TranslatorUpdate

--- Space-Hunter-XZ/Assets/Scripts/Translator.cs
+++ Space-Hunter-XZ/Assets/Scripts/Translator.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Translator : MonoBehaviour
 {
-
     public Text credits;
     public Text creditsButtonText;
     public Text creditBackButtonText;
@@ -19,10 +19,18 @@ public class Translator : MonoBehaviour
     public Text exitText;
     public Text rButtonText;
     public Text bmButtonText;
+    public Text creditsMusicText;
+    private string selectedLanguage;
+    private Scene scene;
+    private string currentScene;
     // Start is called before the first frame update
     void Start()
     {
-       
+        selectedLanguage = PlayerPrefs.GetString("Language");
+        scene = SceneManager.GetActiveScene();
+        currentScene = scene.name;
+        ChangeLanguage();
+        
     }
 
     // Update is called once per frame
@@ -31,25 +39,68 @@ public class Translator : MonoBehaviour
         
     }
 
+
+    public void ChangeLanguage()
+    {
+        if (currentScene.Equals("Main"))
+        {
+            if (selectedLanguage.Equals("English"))
+            {
+                ChangeMainTextEnglish();
+            }
+            else if (selectedLanguage.Equals("Swedish"))
+            {
+                ChangeMainTextSwedish();
+            }
+        }
+        else if (currentScene.Equals("MainMenu"))
+        {
+            if (selectedLanguage.Equals("English")) ChangeMenuTextEnglish();
+            else if (selectedLanguage.Equals("Swedish")) ChangeMenuTextSwedish();
+        }
+    }
+
+    public void SaveData(string language)
+    {
+        selectedLanguage = language;
+        PlayerPrefs.SetString("Language", selectedLanguage);
+        PlayerPrefs.Save();
+    }
+
     public void ChangeMenuTextEnglish(){
-        exitText.text = "EXIT";
-        startText.text = "START";
-        scoreText.text = "Score: ";
-        gameOverText.text = "Game Over!";
-        rButtonText.text = "RESTART";
-        bmButtonText.text = "MAIN MENU";
-        Debug.Log("Changed");
+        if (currentScene.Equals("MainMenu"))
+        {
+            credits.text = "GAME DEVELOPMENT:\n\n\n ANGEL CARDENAS MARTINEZ\n JACOB WIK\n SAMUEL SEGAWA";
+            creditsMusicText.text = "MUSIC\n\n THREE CHAIN LINKS\n threechainlinks.bandcamp.com\n ERIC MATYAS\n www.soundimage.org";
+            creditsButtonText.text = "CREDITS";
+            highScoreButtonText.text = "HIGH SCORE";
+            startText.text = "START GAME";
+            exitText.text = "EXIT";
+            scoreText.text = "SCORE: ";
+            creditBackButtonText.text = "BACK";
+            highScoreText.text = "TOP SCORE";
+            resetScoreButtonText.text = "RESET";
+            highScoreButtonBackText.text = "BACK";
+            SaveData("English");
+        }
     }
 
     public void ChangeMenuTextSwedish(){
-        exitText.text = "Avsluta";
-        startText.text = "Starta";  
-        scoreText.text = "Poäng";
-        gameOverText.text = "Slut!";
-        rButtonText.text = "STARTA OM";
-        bmButtonText.text = "HUVUD MENY";
-       
-        Debug.Log("Ändrad");
+        if (currentScene.Equals("MainMenu")){
+            credits.text = "SPEL UTVECKLING:\n\n\n ANGEL CARDENAS MARTINEZ\n JACOB WIK\n SAMUEL SEGAWA";
+            creditsMusicText.text = "MUSIC\n\n THREE CHAIN LINKS\n threechainlinks.bandcamp.com\n ERIC MATYAS\n www.soundimage.org";
+            creditsButtonText.text = "MEDVERKANDE";
+            highScoreButtonText.text = "HÖGSTA POÄNG";
+            startText.text = "STARTA SPEL";
+            exitText.text = "AVSLUTA";
+            scoreText.text = "POÄNG: ";
+            creditBackButtonText.text = "TILLBAKA";
+            highScoreText.text = "HÖSTA POÄNG";
+            resetScoreButtonText.text = "STARTA OM";
+            highScoreButtonBackText.text = "TILLBAKA";
+            SaveData("Swedish");
+            Debug.Log("Ändrad");
+        }
     }
 
      public void ChangeMainTextEnglish(){
@@ -57,7 +108,7 @@ public class Translator : MonoBehaviour
         gameOverText.text = "Game Over!";
         rButtonText.text = "RESTART";
         bmButtonText.text = "MAIN MENU";
-        Debug.Log("Changed");
+        SaveData("English");
     }
 
     public void ChangeMainTextSwedish(){
@@ -65,6 +116,7 @@ public class Translator : MonoBehaviour
         gameOverText.text = "Slut!";
         rButtonText.text = "STARTA OM";
         bmButtonText.text = "HUVUD MENY";
+        SaveData("Swedish");
         Debug.Log("Ändrad");
     }
 }
