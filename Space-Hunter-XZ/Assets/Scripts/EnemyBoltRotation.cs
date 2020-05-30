415f093182c3cf9282256375cf48f33453de0c9c
Samme <31141459+samkseg@users.noreply.github.com>
2020-05-30 17:09:16 +0200

    bolts

---
+++ Space-Hunter-XZ/Assets/Scripts/EnemyBoltRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoltRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quaternion rotation = new Quaternion(0, 0, 0, 0);
+        transform.rotation = rotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
