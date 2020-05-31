ee80688661a6d9b3ffb29c8f5019fa055b90989e
Samme <31141459+samkseg@users.noreply.github.com>
2020-05-31 15:06:56 +0200

    updatestayisnide

--- Space-Hunter-XZ/Assets/Scripts/StayInside.cs
+++ Space-Hunter-XZ/Assets/Scripts/StayInside.cs
@@ -7,6 +7,6 @@ public class StayInside : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.4f, 2.4f),
-            Mathf.Clamp(transform.position.y, -0.2f, 8f), transform.position.z);
+            Mathf.Clamp(transform.position.y, -4.2f, 4f), transform.position.z);
 	}
 }
